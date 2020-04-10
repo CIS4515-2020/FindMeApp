f4fc23b32452b81cb710453e5019615121eaaf57
Samuel Hong <tug67936@temple.edu>
2020-04-09 22:28:16 -0400

    Create login and register dialogs on button click

--- app/src/main/java/edu/temple/findmeapp/MainActivity.java
+++ app/src/main/java/edu/temple/findmeapp/MainActivity.java
@@ -2,11 +2,16 @@ package edu.temple.findmeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity ===>>>";
@@ -18,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonLogin;
     private Button buttonRegister;
+    private AlertDialog dialogLogin;
+    private AlertDialog dialogRegister;
+
+    private String email;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +36,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonNewItemActivity = findViewById(R.id.button_new_item);
-        buttonEditItemActivity = findViewById(R.id.button_edit_item);
-        buttonDeleteItemActivity = findViewById(R.id.button_delete_item);
+        buttonNewItemActivity     = findViewById(R.id.button_new_item);
+        buttonEditItemActivity    = findViewById(R.id.button_edit_item);
+        buttonDeleteItemActivity  = findViewById(R.id.button_delete_item);
         buttonItemDisplayActivity = findViewById(R.id.button_item_display);
+        buttonLogin               = findViewById(R.id.button_login);
+        buttonRegister            = findViewById(R.id.button_register);
 
         buttonNewItemActivity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,11 +92,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showLoginDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_login, null);
 
+        EditText editTextLoginUsername = view.findViewById(R.id.loginDialogUsername);
+        EditText editTextLoginPassword = view.findViewById(R.id.loginDialogPassword);
+        Button   buttonLoginDialog     = view.findViewById(R.id.loginDialogButton);
+
+        buttonLoginDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO send login request to server
+                // TODO save login info
+                dialogLogin.cancel();
+            }
+        });
+
+        builder.setView(view);
+        dialogLogin = builder.create();
+        dialogLogin.show();
     }
 
     private void showRegisterDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_register, null);
 
+        EditText editTextRegisterUsername = view.findViewById(R.id.registerDialogUsername);
+        EditText editTextRegisterEmail    = view.findViewById(R.id.registerDialogEmail);
+        EditText editTextRegisterPassword = view.findViewById(R.id.registerDialogPassword);
+        Button   buttonRegisterDialog     = view.findViewById(R.id.registerDialogButton);
+
+        buttonRegisterDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO send registration request to server
+                dialogRegister.cancel();
+            }
+        });
+
+        builder.setView(view);
+        dialogRegister = builder.create();
+        dialogRegister.show();
     }
 
 
