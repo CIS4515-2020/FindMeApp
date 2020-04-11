602939afd581705a0c6e23808d41cbd164148d50
Samuel Hong <tug67936@temple.edu>
2020-04-10 23:00:50 -0400

    Add FoundItemMessage class

--- app/src/main/java/edu/temple/findmeapp/Item.java
+++ app/src/main/java/edu/temple/findmeapp/Item.java
@@ -1,14 +1,25 @@
 package edu.temple.findmeapp;
 
+import java.util.ArrayList;
+
 public class Item {
     private int id;
     private String name;
     private String description;
+    private ArrayList<FoundItemMessage> foundItemMessages;
 
     public Item(int id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.foundItemMessages = new ArrayList<FoundItemMessage>();
+    }
+
+    public Item(int id, String name, String description, ArrayList<FoundItemMessage> foundItemMessages) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.foundItemMessages = foundItemMessages;
     }
 
     public int getId() {
@@ -33,5 +44,21 @@ public class Item {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public ArrayList<FoundItemMessage> getFoundItemMessages() {
+        return foundItemMessages;
+    }
+
+    public void setFoundItemMessages(ArrayList<FoundItemMessage> foundItemMessages) {
+        this.foundItemMessages = foundItemMessages;
+    }
+
+    public void addFoundItemMessage(FoundItemMessage foundItemMessage) {
+        this.foundItemMessages.add(foundItemMessage);
+    }
+
+    public void clearFoundItemMessages() {
+        this.foundItemMessages.clear();
     }
 }
